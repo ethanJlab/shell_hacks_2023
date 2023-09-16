94ce4ee5182f6bcbcb9f2f4e4ca38d18ee85c7b7
sebsours <86129880+sebsours@users.noreply.github.com>
2023-09-16 16:50:12 -0400

    Added Auth0 functionality

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import './index.css'
 import LandingPage from './pages/Landing.tsx'
 import DashboardPage from './pages/Dashboard.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const kevsRouter = createBrowserRouter([
   {
@@ -25,6 +26,14 @@ const kevsRouter = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={kevsRouter}/>
+    <Auth0Provider
+      domain='dev-xhc2dcreaqasyjnj.us.auth0.com'
+      clientId='h7C3ao2SQuA5pX2I54fP2Pa7lER1lbd3'
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}>
+      <RouterProvider router={kevsRouter}/>
+    </Auth0Provider>
+    
   </React.StrictMode>,
 )
