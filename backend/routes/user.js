d585d82ae5c9e63f7feef1fc26aab9310eac03ac
ethanJlab <ethan.janicki@ucf.edu>
2023-09-16 01:14:23 -0700

    feat: add user endpoints

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,7 +10,7 @@ export const userRouter = express.Router();
 // POST /user
 // the data should contain first_name, last_name, password, income, location, Debt_IDs, and investment_IDs
 
-userRouter.post('/', (req, res) => {
+userRouter.post('/create', (req, res) => {
     console.log(req.body)
     pb.collection('user').create(req.body).then((user) => {
         res.json(user);
@@ -18,5 +18,28 @@ userRouter.post('/', (req, res) => {
         res.json(err);
     });
 });
+
+// get a user by id
+// GET /user/:id
+userRouter.get('/:id', (req, res) => {
+    pb.collection('user').getOne(req.params.id).then((user) => {
+        res.json(user);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+// update
+// PUT /user/:id
+userRouter.put('/:id', (req, res) => {
+    pb.collection('user').update(req.params.id, req.body).then((user) => {
+        res.json(user);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+
+
 
 export default userRouter;
