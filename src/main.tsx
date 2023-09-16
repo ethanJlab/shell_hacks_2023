5181e3d51c2a3cc48c05fa9c1c6c673dbaace64b
KevinKant210 <kevinkant@knights.ucf.edu>
2023-09-16 03:30:09 -0400

    Added Routing to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import{createBrowserRouter,RouterProvider} from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import LandingPage from './pages/Landing.tsx'
+import DashboardPage from './pages/Dashboard.tsx'
+
+const kevsRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <LandingPage/>,
+  },
+
+  {
+    path:"/App",
+    element:<App/>
+  },
+
+  {
+    path:"/Dashboard",
+    element: <DashboardPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={kevsRouter}/>
   </React.StrictMode>,
 )
