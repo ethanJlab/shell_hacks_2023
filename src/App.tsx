f289ab85ecb2a822cf58a6632d84c8cb0a900728
sebsours <86129880+sebsours@users.noreply.github.com>
2023-09-16 01:36:37 -0400

    Vite stuff :DDD

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <span>HELLO WORLD SEB'S VITE IS IN THE REPO!!!</span>
+  )
+}
+
+export default App
