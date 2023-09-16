d49041a10d85f1bc7c9e384da8a01a77b557b628
sebsours <86129880+sebsours@users.noreply.github.com>
2023-09-16 02:04:22 -0400

    Added tailwindcss, MUI, react-router, and chartjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <span>HELLO WORLD SEB'S VITE IS IN THE REPO!!!</span>
+    <span className='text-9xl text-red-500 font-bold'>HELLO WORLD SEB'S VITE IS IN THE REPO!!!</span>
   )
 }
 
