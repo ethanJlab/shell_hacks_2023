a9ff1c3cdf4cee1d5847fe798a27a463d23171f3
KevinKant210 <kevinkant@knights.ucf.edu>
2023-09-17 03:10:04 -0400

    Quick Commit for structure bfore auth

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,51 @@
-import{ ReactElement } from "react";
-import {Link} from 'react-router-dom'
+import { ReactElement, useState } from "react";
+import { TextField,Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function LandingPage():ReactElement{
+export default function LandingPage(): ReactElement {
+  const [userName, setUserName] = useState("");
+  const [userPass, setUserPass] = useState("");
+  const [isLoading,setIsLoading] = useState(false);
 
-    const routeToApp = () => {
+   const handleLogin = async () => {
+    
+    //do user auth check here
+    //re rout to the dashboard
+    
+    setIsLoading(true);
+    
+    setIsLoading(false);
+   }
 
-    }
-    return(
-        <div className="">
-            <p className="text-white">This is our landing page</p>
+  const routeToApp = () => {};
+  return (
+    <div className="flex  justify-center items-center h-full">
+      <div className="bg-neutral-900 flex flex-col w-1/2 h-1/3 justify-center items-center gap-10 p-5 rounded-xl">
+        <div>
+          <p className="text-[#FAF9F6] text-6xl">Login</p>
+        </div>
+
+        <div>
+          <TextField label="Email"   className="bg-[#FAF9F6] rounded-xl " required color="success" value={userName} onChange={(e) => setUserName(e.target.value)} />
+        </div>
+        
+        <div>
+            <TextField label="Password" className="bg-[#FAF9F6] rounded-xl" required type="password" color="success" onChange={(e) => setUserPass(e.target.value)}/>
+        </div>
+
+        <div>
+            <Button variant="contained" className="bg-[#FAF9F6]" onClick={handleLogin}>{ isLoading ? "Loading..." : "Submit"}</Button>
+        </div>
+
+        <div>
+            <a className="text-[#FAF9F6] text-2xl "  >New? Come take your first step towards financial literacy </a>
+        </div>
+      </div>
+      {/* <p className="text-white">This is our landing page</p>
 
             <a><Link to={"/App"} className="text-white"> Go To App :)</Link></a>
             <br/>
-            <a><Link to={"/Dashboard"} className="text-white">Go To Dashboard</Link></a>
-        </div>
-    )
+            <a><Link to={"/Dashboard"} className="text-white">Go To Dashboard</Link></a> */}
+    </div>
+  );
 }
