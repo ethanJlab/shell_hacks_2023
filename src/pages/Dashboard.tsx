fef9f18d003872d8c92ca3966be5e3ce59f036ca
KevinKant210 <kevinkant@knights.ucf.edu>
2023-09-17 10:56:38 -0400

    Last update :DD

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -28,7 +28,7 @@ export default function DashboardPage(props: any): React.ReactElement {
     debtAmount: 600,
   };
 
-  const defaultIncome = 55000
+  const defaultIncome = 55000;
 
   useEffect(() => {
     //here is where we really need to gather some of this user info
@@ -80,11 +80,19 @@ export default function DashboardPage(props: any): React.ReactElement {
   //   "Entertainment and Leisure": "5%
   //   "Personal Care and Miscellaneous": "5%
 
-  const incomeParts = [(defaultIncome *.25).toFixed(2),(defaultIncome *.15).toFixed(2),(defaultIncome *.15).toFixed(2),
-    (defaultIncome *.10).toFixed(2),(defaultIncome *.10).toFixed(2),(defaultIncome *.05).toFixed(2),(defaultIncome *.10).toFixed(2),
-    (defaultIncome *.05).toFixed(2),(defaultIncome *.05).toFixed(2)]
+  const incomeParts = [
+    (defaultIncome * 0.25).toFixed(2),
+    (defaultIncome * 0.15).toFixed(2),
+    (defaultIncome * 0.15).toFixed(2),
+    (defaultIncome * 0.1).toFixed(2),
+    (defaultIncome * 0.1).toFixed(2),
+    (defaultIncome * 0.05).toFixed(2),
+    (defaultIncome * 0.1).toFixed(2),
+    (defaultIncome * 0.05).toFixed(2),
+    (defaultIncome * 0.05).toFixed(2),
+  ];
 
-    const spent = [14000,5000,5000,7450,300,200,1000,1000]
+  const spent = [14000, 5000, 5000, 7450, 300, 200, 1000, 1000];
 
   const labels = [
     "Housing",
@@ -97,8 +105,6 @@ export default function DashboardPage(props: any): React.ReactElement {
     "Miscellaneous",
   ];
 
-
-
   const data = {
     labels,
     datasets: [
@@ -109,7 +115,7 @@ export default function DashboardPage(props: any): React.ReactElement {
       },
       {
         label: "Max",
-        data: labels.map((currItem,index) => incomeParts[index]),
+        data: labels.map((currItem, index) => incomeParts[index]),
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
@@ -119,8 +125,53 @@ export default function DashboardPage(props: any): React.ReactElement {
     <div className="w-full h-screen bg-black flex">
       {/* Section For the GPT Recommendation */}
       <div className="h-full w-1/3 bg-neutral-900 rounded-lg m-3.5 align-self-start">
-        <div className="bg-neutral-700  h-5/6 m-5 rounded-xl ">
-          <Button>Generate Tip</Button>
+        <div className="bg-neutral-700 min-h-[70vh] m-5 rounded-xl  p-2 text-[#FAF9F6] font-medium leading-loose flex flex-col gap-5">
+          <p>
+            <strong>Budget Your Way to Freedom:</strong> Start by creating a
+            detailed budget to track your income and expenses. This will give
+            you a clear picture of where your money is going and where you can
+            cut back.
+          </p>
+
+          <p>
+            <strong>Prioritize High-Interest Debt:</strong> Tackle your credit
+            card debt with the highest interest rates first. Paying down these
+            balances faster will save you money in the long run.
+          </p>
+
+          <p>
+            <strong>Cut Non-Essential Expenses:</strong> Identify areas where
+            you can reduce spending, such as dining out or subscription
+            services. Redirect the money you save towards debt repayment.
+          </p>
+
+          <p>
+            <strong>Emergency Fund:</strong> Build an emergency fund to cover
+            unexpected expenses. Having this safety net will prevent you from
+            relying on credit cards in times of crisis.
+          </p>
+
+          <p>
+            <strong>Snowball Method:</strong> Consider using the debt snowball
+            method, focusing on paying off smaller debts first. The sense of
+            accomplishment from clearing these debts can motivate you to tackle
+            larger ones.
+          </p>
+
+          <p>
+            <strong>Seek Professional Guidance:</strong> Don't hesitate to
+            consult a financial advisor or credit counselor. They can provide
+            expert advice tailored to your situation and help you make informed
+            decisions.
+          </p>
+
+          <p>
+            Remember, every step you take toward financial freedom is a step in
+            the right direction. You've got this! 🌟
+          </p>
+          <Button variant="primary" className="mt-10">
+            Generate Tips
+          </Button>
         </div>
       </div>
       {/* Right half of screen that contains Debts Investments and Budget */}
@@ -132,10 +183,12 @@ export default function DashboardPage(props: any): React.ReactElement {
 
             <p className="text-[#FAF9F6] text-lg">
               You lose {(defaultDebt.debtAmount * 0.017).toFixed(2)} every month
-              you ignore your debt,<br/> Learn more{" "}
-              <a 
+              you ignore your debt,
+              <br /> Learn more{" "}
+              <a
                 className=" font-semibold italic text-green-400"
-                href="https://retirementplans.vanguard.com/VGApp/pe/pubeducation/spending/ManageDebtSpendOV.jsf?SelectedSegment=BuildingWealth&Topic=Manage+debt+and+spending">
+                href="https://retirementplans.vanguard.com/VGApp/pe/pubeducation/spending/ManageDebtSpendOV.jsf?SelectedSegment=BuildingWealth&Topic=Manage+debt+and+spending"
+              >
                 here
               </a>
             </p>
@@ -144,10 +197,12 @@ export default function DashboardPage(props: any): React.ReactElement {
             <InvestGraph />
             <p className="text-[#FAF9F6] text-lg">
               You gain {(defaultDebt.debtAmount * 0.017).toFixed(2)} every month
-              you have kept your investment,<br/> Learn more{" "}
-              <a 
+              you have kept your investment,
+              <br /> Learn more{" "}
+              <a
                 className=" font-semibold italic text-green-400"
-                href="https://investor.vanguard.com/corporate-portal">
+                href="https://investor.vanguard.com/corporate-portal"
+              >
                 here
               </a>
             </p>
